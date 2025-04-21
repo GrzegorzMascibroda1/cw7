af0c83545daf6a48db9d26e54df0a3c222710ddc
GrzegorzMascibroda1 <s33341@pjwstk.edu.pl>
2025-04-21 15:57:23 +0200

    zadania cw7

--- Animal.java
+++ Animal.java
@@ -3,12 +3,10 @@ public class Animal {
         System.out.println("MakeNoise");
     }
 
-    public void sleep(){
+    public void sleep() {
         System.out.println("Sleeping");
     }
-    public void eat(){
-        System.out.println("Eating");
-    }
+
     public void roam(){
         System.out.println("Roaming");
     }
